e80359218c7084db3ce4a057e40284b33a5c5015
ltcxjtu <1440935236@qq.com>
2017-07-13 10:18:40 +0800

    2017-07-13

--- egs/console.m
+++ egs/console.m
@@ -14,59 +14,34 @@
 addpath('../enhan');
 addpath('../utils');
 
-clear all;
-
+clear;
+micPosition=[0,-0.0385,0;
+             0,+0.0385,0;
+             -0.033341,-0.01925,0;
+             0.033341,0.01925,0;
+             -0.033341,+0.01925,0;
+             0.033341,-0.01925,0;
+             ];
 Nsource = 2;
 Nchan = 6;
 Lwindow = 256;
 Nfft = Lwindow;
-overlap = 0.75;
+overlap = 0.5;
 powThresh = -10;
 cmin = 6400; % minimum context duration (400 ms)
 cmax = 12800; % maximum context duration (800 ms)
 EMITERNUM = 20;
 GAMMA = 20;
-
-dirn = 'C:\Users\mi\Downloads\myairbridge-FDXXE174Y3RM9Q25\CHiME3_isolated_et05_real\CHiME3\data\audio\16kHz\isolated\et05_bus_real\';
-firname= 'F05_440C020E_BUS';
-% Load WAV files
-xsize = size(audioread([dirn firname '.CH1.wav']));
-x = zeros(xsize(1),Nchan);
-xc = zeros(xsize(1),Nchan);
-for clp = 1:Nchan
-    [x(:,clp),fs] = audioread([dirn firname '.CH' int2str(clp) '.wav']);
-    xc(:,clp) = x(:,clp) / norm(x(:,clp)) * norm(x(:,1));
-end
-
-% Check microphone failure
-CLwindow = 2048;
-Coverlap = 0.5;
-CNfft = CLwindow;
-[~,~,~,~,CspeechFrame] =  STFT(xc, CLwindow, Coverlap, CNfft);
-CspeechFrame = permute(CspeechFrame, [3,1,2]);
-NcorFFT = 2 * (CNfft-1);
-c1 = fft(repmat(permute(CspeechFrame, [1,4,2,3]),[1 Nchan 1 1]), NcorFFT, 3);
-c2 = fft(repmat(permute(CspeechFrame, [4,1,2,3]),[Nchan 1 1 1]), NcorFFT, 3);
-corr = real(ifft(c1 .* conj(c2),NcorFFT,3));
-corr = squeeze(max(corr, [], 3));
-
-corrjudge = sum(corr, 2) - sum(bsxfun(@times,corr,eye(Nchan)),2);
-corrjudge = squeeze(corrjudge);
-corrjudge_rate = bsxfun(@rdivide, corrjudge, median(corrjudge,1));
-fail = corrjudge_rate < 0.6;
-fail = (sum(fail,2) > 1);
-fail = fail';
-if all(fail)
-    fail = (sum(corrjudge_rate,2) == max(sum(corrjudge_rate,2)));
-    fail = ~fail;
-    fail = fail';
-end
-% choose reference mic
-[~,refMic] = max(sum(corrjudge(~fail,:),2));          
-
-Nchan = sum(~fail);
-x = x(:,~fail);
-            
+sampRate=16e3;
+dirn='./';
+filename = 'person1_scene1_music1_level1_angle2_-135rad_noise7_SNR15dB_6_sim_words01.wav';
+ xsize = size(audioread([dirn filename]));
+ xc = zeros(xsize(1),Nchan);
+ x = audioread([dirn filename]);
+ for clp = 1:Nchan
+       xc(:,clp) = x(:,clp) / norm(x(:,clp)) * norm(x(:,1));
+ end
+                  
 [ftbin,Nframe,Nbin,Lspeech] =  STFT(x, Lwindow, overlap, Nfft);   
 % for GSC fixed beamformer
 targetY = squeeze(mean(ftbin,1));
@@ -82,8 +57,44 @@ softmask = cGaussMask(ftbin,Nsource,XX,EMITERNUM);
 Ncor = bsxfun(@rdivide, mean(bsxfun(@times, XX, ...
     permute(softmask(:,:,2),[3,4,1,2])),4),permute(mean(softmask(:,:,2),2), [2,3,1]));                       
 Gcor = Xcor - Ncor;
-Y = MVDR_EV(ftbin, Gcor, Ncor);
-%for bssMvdrEg
+% create template to find the DOA; 
+numberLevels = 3;
+mySphere=sphereInit(numberLevels);
+
+% create steering vector template;
+[a,numbftbin,c]=size(ftbin);
+waveSpeed=343;
+steerVec=zeros(Nchan,numbftbin,mySphere.SPHERE_NUMBERPOINTS);
+
+%get the steering vector;
+%  steeringVector(xPos, yPos, zPos, f, c, thetaScanAngles, phiScanAngles)
+
+for j=1:mySphere.SPHERE_NUMBERPOINTS
+    delay = sqrt(sum((mySphere.spherePoints(j,:)-micPosition).*...
+    (mySphere.spherePoints(j,:)-micPosition),2))/waveSpeed;%broadcast
+    delay=delay-min(delay);
+    for index=1:numbftbin
+        f=(index-1)*(sampRate/2)/(Lwindow/2);
+        steerVec(:,index,j)=exp(-2*pi*1i*f*delay);
+    end    
+end
+
+Pmatch=zeros(mySphere.SPHERE_NUMBERPOINTS,4);
+Pmatch(:,1:3) = mySphere.spherePoints;
+[Y,Df] = MVDR_EV(ftbin, Gcor, Ncor);
+
+% template match
+for j=1:mySphere.SPHERE_NUMBERPOINTS
+%     Pmatch(j,4) = sum(sum((Df.*permute(steerVec(:,:,j),[1,2]))).*...
+%         conj(sum(Df.*permute(steerVec(:,:,j),[1,2]))));
+%      Pmatch(j,4) = norm(sum(sum(abs((Df.*permute(steerVec(:,:,j),[1,2]))))),2);    
+        Pmatch(j,4) = sum(sum((Df.*conj(permute(steerVec(:,:,j),[1,2])))).*...
+        conj(sum(Df.*conj(permute(steerVec(:,:,j),[1,2])))));
+end
+        
+
+    
+%for bssMvdrEgyBssMvdrEgFt = MVDR_EV(ftbin, Gcor, N 
 yBssMvdrEgFt = MVDR_EV(ftbin, Gcor, Ncor);
 yBssMvdrEg = ISTFT(yBssMvdrEgFt,Lwindow,overlap);
 yBssMvdrEg = yBssMvdrEg / max(abs(yBssMvdrEg));
---
+++ egs/consoleforDOA03.m
@@ -0,0 +1,126 @@
+% speech enhancement for CHiME4 6-ch data
+% corresponding to "The THU-SPMI CHiME-4 system : Lightweight design with
+% advanced multi-channel processing, feature enhancement, and language modeling"
+
+% based on CHiME3 official code
+% Jon Barker, Ricard Marxer, Emmanuel Vincent, and Shinji Watanabe, The
+% third 'CHiME' Speech Separation and Recognition Challenge: Dataset,
+% task and baselines, submitted to IEEE 2015 Automatic Speech Recognition
+% and Understanding Workshop (ASRU), 2015.
+
+% This software is distributed under the terms of the GNU Public License
+% version 3 (http://www.gnu.org/licenses/gpl.txt)
+
+addpath('../enhan');
+addpath('../utils');
+
+clear;
+micPosition=[0,-0.0385,0;
+             0,+0.0385,0;
+             -0.033341,-0.01925,0;
+             0.033341,0.01925,0;
+             -0.033341,+0.01925,0;
+             0.033341,-0.01925,0;
+             ];
+         
+% create template to find the DOA; 
+numberLevels = 3;
+mySphere=sphereInit(numberLevels);
+
+Nsource = 2;
+Nchan = 6;
+Lwindow = 256;
+Nfft = Lwindow;
+overlap = 0.5;
+powThresh = -10;
+EMITERNUM = 20;
+GAMMA = 20;
+sampRate=16e3;
+num_Fbin = round(Lwindow/2)+1;
+
+
+% create steering vector template;
+waveSpeed=343;
+steerVec=zeros(Nchan,num_Fbin,mySphere.SPHERE_NUMBERPOINTS);
+
+%get the steering vector;
+for j=1:mySphere.SPHERE_NUMBERPOINTS
+    delay = sqrt(sum((mySphere.spherePoints(j,:)-micPosition).*...
+    (mySphere.spherePoints(j,:)-micPosition),2))/waveSpeed;%broadcast
+    delay=delay-min(delay);
+    for index=1:num_Fbin
+        f=(index-1)*(sampRate/2)/(Lwindow/2);
+        steerVec(:,index,j)=exp(-2*pi*1i*f*delay);
+    end    
+end
+
+dirn='D:\out16k\out16kdata\';
+filename = dir(dirn);
+
+for indexFile = 1:length(filename)
+    if exist(filename(indexFile).name,'file')
+        continue;
+    end
+     data = audioread([dirn filename(indexFile).name]);
+     xsize = size(data);
+     
+     % segment data to get DOA by mask
+     len_Batch = 4096;
+     batch_OverLap = 0.5;
+     number_Lwin = round(xsize(1)/(Lwindow*(1-overlap)));
+     angleSet = zeros(number_Lwin-30,1);
+     for Index = 1:number_Lwin-30
+        try
+             x = data(1+(Index-1)*Lwindow*(1-overlap)...
+                     :len_Batch+(Index-1)*Lwindow*(1-overlap),:);
+            [ftbin,Nframe,Nbin,Lspeech] =  STFT(x, Lwindow, overlap, Nfft);   
+
+            % correlation of X
+            XX = bsxfun(@times, permute(ftbin,[1,4,2,3]), conj(permute(ftbin,[4,1,2,3])));
+
+            Xcor = mean(XX, 4);
+            softmask = cGaussMask(ftbin,Nsource,XX,EMITERNUM);
+            Ncor = bsxfun(@rdivide, mean(bsxfun(@times, XX, ...
+                permute(softmask(:,:,2),[3,4,1,2])),4),permute(mean(softmask(:,:,2),2), [2,3,1]));                       
+            Gcor = Xcor - Ncor;
+
+            %match_values of SpherePoints; 
+            Pmatch=zeros(mySphere.SPHERE_NUMBERPOINTS,4);
+            Pmatch(:,1:3) = mySphere.spherePoints;
+
+            % get the steering vector which is estimated by time-freq mask,
+            % meanwhile the time-freq mask is estimated by CGMM;
+            [Y,Df] = MVDR_EV(ftbin, Gcor, Ncor);
+
+            % template match
+            for j=1:mySphere.SPHERE_NUMBERPOINTS
+                    Pmatch(j,4) = sum(sum((Df.*conj(permute(steerVec(:,:,j),[1,2])))).*...
+                    conj(sum(Df.*conj(permute(steerVec(:,:,j),[1,2])))));
+            end
+
+            % plot the sphere;
+            %scatter3(Pmatch(:,1),Pmatch(:,2),Pmatch(:,3),256,Pmatch(:,4));
+
+            %find the DOA from Spherepoints
+            index = find(Pmatch(:,4) == max(Pmatch(:,4)));
+            bestPoint = mySphere.spherePoints(index,:);
+            angle = atan(abs(bestPoint(2)/bestPoint(1)))*180/pi;
+            if bestPoint(1) < 0 &&bestPoint(2) < 0
+                angle = -angle-90;
+            end
+            if bestPoint(1) < 0 &&bestPoint(2) > 0
+                angle = angle+90;
+            end
+            if bestPoint(1) > 0 &&bestPoint(2) < 0
+                angle = -angle;
+            end
+            angleSet(Index) = angle;
+        %     fprintf('angle:%f\n',angle);    
+        end
+        continue;
+     end
+     save([dirn filename(indexFile).name num2str(len_Batch) '.mat'],'angleSet');
+ end
+
+
+
